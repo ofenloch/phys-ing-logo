1f5d87d127a6fc9fe7e2faab96b1b0d2098eea73
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-07 16:07:54 +0100

    format file, remove unused stuff

--- make.sh
+++ make.sh
@@ -9,7 +9,7 @@ for f in phys-ing*.tex ; do
   echo "filename is ${f}"
   echo "name is ${name}"
   # create dvi from tex:
-  latex  --shell-escape ${f};
+  latex --shell-escape ${f};
   # create svg from dvi:
   dvisvgm --no-fonts ${name}.dvi -o ${name}.svg;
   # create various png files from the svg file:
@@ -19,12 +19,11 @@ for f in phys-ing*.tex ; do
   inkscape -z -e ${name}-30x90px.png -h 30 -w 90 ${name}.svg;
   inkscape -z -e ${name}-32x32px.png -h 32 -w 32 ${name}.svg;
   inkscape -z -e ${name}-48x48px.png -h 48 -w 48 ${name}.svg;
-   inkscape -z -e ${name}-50x150px.png -h 50 -w 150 ${name}.svg;
+  inkscape -z -e ${name}-50x150px.png -h 50 -w 150 ${name}.svg;
   inkscape -z -e ${name}-100x100px.png -h 100 -w 100 ${name}.svg;
   inkscape -z -e ${name}-100x300px.png -h 100 -w 300 ${name}.svg;
   inkscape -z -e ${name}-200x600px.png -h 200 -w 600 ${name}.svg;
   inkscape -z -e ${name}-300x900px.png -h 300 -w 900 ${name}.svg;
   inkscape -z -e ${name}-500x1500px.png -h 500 -w 1500 ${name}.svg;
 done;
-inkscape -z -e favicon-48x48px.png -h 48 -w 48 favicon.svg;
-inkscape -z -e logo-100x300px.png -h 100 -w 300 logo.svg;
+
